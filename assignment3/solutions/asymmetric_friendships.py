4e8e85efef20d4b3d9447ebfa60a4d994233adba
davislf2.net@gmail.com <davislf2.net@gmail.com>
2018-11-24 22:15:52 +1100

    finished assignment 3 problem 4

--- assignment3/solutions/asymmetric_friendships.py
+++ assignment3/solutions/asymmetric_friendships.py
@@ -24,47 +24,31 @@ mr = MapReduce.MapReduce()
 # =============================
 # Do not modify above this line
 
-from itertools import permutations
 
 def mapper(record):
-    # key: document identifier
-    # value: document contents
+    # key  : person
+    # value: friend
     person = record[0]
     friend = record[1]
-    friendship_pair = [person, friend]
-    mr.emit_intermediate(person, friendship_pair)
+    mr.emit_intermediate(person, friend)
 
 
-def reducer(key, list_of_pairs):
-    # key: word
-    # value: list of occurrence counts
-    set_of_pairs = set(list_of_pairs)
-    try:
-        # names = mr.intermediate[key][3]
-
-        # tot_pairs = list(permutations())
-        for p in set_of_pairs:
-            p[0], p[1] = p[1], p[0]
-            mr.intermediate[key].append(p)
-        for k, v in mr.intermediate.iteritems():
-            mr.intermediate[k] = v[2] - set_of_pairs
-    except:
-
-    mr.result
-    total_pairs = set()
-    names = set()
-    for pair in list_of_values:
-        if [pair[1], pair[0]] in total_pairs:
-            total_pairs.remove([pair[1], pair[0]])
-    if key not in names:
-        names.add(key)
-        for n in names:
-            if [n, key] not in total_pairs:
-                total_pairs.add([n, key])
-    list_of_values
-    # for v in list_of_values:
-    #     total += v
-    mr.emit((key, total))
+def reducer(person, list_of_friends):
+    # key: person
+    # value: list of friends
+    print person, list_of_friends
+    for friend in list_of_friends:
+        try:
+            # retrieve the friend's list_of_friends
+            friends_list_of_friends = mr.intermediate[friend]
+            # the friend doesn't connect back to the person
+            if person not in friends_list_of_friends:
+                mr.emit((person, friend))
+                mr.emit((friend, person))
+        # the friend doesn't have outer connection (list_of_friends)
+        except KeyError:
+                mr.emit((person, friend))
+                mr.emit((friend, person))
 
 
 # Do not modify below this line
