6cfe0abaed7fcc3479aaa4150c64ccc55c6fa9d3
davislf2.net@gmail.com <davislf2.net@gmail.com>
2018-11-25 19:11:19 +1100

    modified ass3 p1&p4, finished ass1 p1

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -2,47 +2,43 @@ import sys
 import json
 import copy
 
-def hw(sent_file, tweet_file):
-    scores, len_sent = get_scores(sent_file)
-    # print(scores.items()) # Print every (term, score) pair in the dictionary
-    # with open(tweet_file) as f:
-    #     data = f.readlines()
+def getScoresOfSentiment(sent_file):
+    scores = {}
+    len = 0
+    for line in sent_file:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+        len += 1
+    return scores, len
 
-    # tweet_file_copy = copy.deepcopy(tweet_file)
+
+def hw(sent_file, tweet_file):
+    word_scores, len_sent = getScoresOfSentiment(sent_file)
+    # print(word_scores.items()) # Print every (term, score) pair in the dict
+
     count = 0
     tweet_score = {}
     for line in tweet_file:
-    # for line in tweet_file_copy:
-        
-        if count < 10:
-            # print(line)
-            try:
-                tweet = json.loads(line) #.decode("utf-8")
-                # print(tweet["text"])
-                # tweet_score[tweet["text"]] = 0
-                tweet_score = 0
-                # words = tweet["text"].split()
-                # print [w.encoding for w in words]
-                # print words[0] == 'RT'
-                # if "gain" in scores.keys():
-                #     print "gain"
-
-                for word in tweet["text"].split():
+    #     if count < 100:
+        tweet = json.loads(line)  # .decode("utf-8")
+        try:
+            tweet_score = 0
+            for word in tweet["text"].split(" "):
+                # print word
+                if word in word_scores: #scores.keys():
+                    # tweet_score[tweet["text"]] += scores[word]
+                    tweet_score += word_scores[word]
                     # print word
-                    if word in scores: #scores.keys(): 
-                        # tweet_score[tweet["text"]] += scores[word]
-                        tweet_score += scores[word]
-                        # print word
-                print tweet_score
-                
-            except:
-                pass #print 0
-        else:
-            break
+            print tweet_score
+
+        except:
+            pass  # print 0
+        # else:
+        #     break
         count += 1
     
-    print len_sent
-    print count
+    # print len_sent
+    # print count
     # print count
     # print(tweet_score)
 
@@ -61,15 +57,6 @@ Because sent_file and tweet_file have iterated all lines, so it doesn't has any 
 '''
 # def lines(fp):
 #     return str(len(fp.readlines()))
-
-def get_scores(sent_file):
-    scores = {}
-    len = 0
-    for line in sent_file:
-        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-        scores[term] = int(score)  # Convert the score to an integer.
-        len += 1
-    return scores, len
 
 def main():
     sent_file = open(sys.argv[1])
